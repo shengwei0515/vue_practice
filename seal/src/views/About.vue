c4cde00b2fa41d56df2741e12c9d4486c94c6cd7
shengwei0515 <shengwei199505@gmail.com>
2021-01-03 17:42:16 +0800

    Add event card practice

--- seal/src/views/About.vue
+++ seal/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">About Seal Server</h1>          
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="org-description is-size-4">I dont' know what to say ...</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .org-description {
+    margin-top: 50px;
+  }
+</style>
